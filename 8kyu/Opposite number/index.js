f5d0182caf6bcf5f933a43357f9ca45dcde1f3aa
Triola Jackson <triolajackson@gmail.com>
2022-05-01 10:21:35 -0500

    Create opposite number.js

---
+++ 8kyu/Opposite number/index.js
@@ -0,0 +1,18 @@
+/*
+Title: Opposite number
+Description: Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+// Long Solution
+// function opposite(number) {
+//   return number * -1;
+// }
+
+function opposite(number) {
+  return -number;
+}
