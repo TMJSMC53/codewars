193f35f2b5199df178d8df8d4c0014a20f50d9e0
Triola Jackson <triolajackson@gmail.com>
2022-05-04 10:04:40 -0500

    Create sum of positive.js

---
+++ 8kyu/Sum of positive/index.js
@@ -0,0 +1,12 @@
+/*
+Title: Sum of positive
+Description: You get an array of numbers, return the sum of all of the positives ones.
+
+Examples: [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+*/
+
+function positiveSum(arr) {
+  return arr.filter((num) => num > 0).reduce((sum, num) => sum + num, 0);
+}
