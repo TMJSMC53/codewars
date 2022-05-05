f816bc340f115965b2f1e5cf49946e6a4d784efe
Triola Jackson <triolajackson@gmail.com>
2022-05-04 20:37:58 -0500

    Create filter long words.js

---
+++ 7kyu/Filter Long Words/index.js
@@ -0,0 +1,19 @@
+/*
+Title: Filter Long Words
+Description: Write a function that takes a string and an an integer n as parameters and returns a list of all words that are longer than n.
+
+Example:
+
+* With input "The quick brown fox jumps over the lazy dog", 4
+* Return ['quick', 'brown', 'jumps']
+*/
+
+// Long Solution
+function filterLongWords(sentence, a) {
+  return sentence.split(" ").filter((word) => word.length > n);
+}
+
+// Refactor
+const filterLongWords = (sentence, a) => {
+  return sentence.split(" ").filter((word) => word.length > a);
+};
