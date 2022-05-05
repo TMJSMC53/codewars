d85ab9b834ba057b33e64826e15511e0152732f3
Triola Jackson <triolajackson@gmail.com>
2022-05-05 13:35:17 -0500

    git commit --amend --date="2022-01-02" -m "Add coding challenge to repo"

---
+++ 8kyu/Remove String Spaces/index.js
@@ -0,0 +1,8 @@
+/*
+Title: Remove String Spaces
+Description: Simple, remove the spaces from the string, then return the resultant string.
+*/
+
+function noSpace(x) {
+  return x.split(" ").join("");
+}
