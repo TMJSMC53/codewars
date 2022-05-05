7e2a399aae20995cddad1787ebee36dfb413806e
Triola Jackson <triolajackson@gmail.com>
2022-05-05 13:43:28 -0500

    "Adding coding challenge to repo"

--- 8kyu/Remove String Spaces/index.js
+++ 8kyu/Remove String Spaces/index.js
@@ -3,6 +3,7 @@ Title: Remove String Spaces
 Description: Simple, remove the spaces from the string, then return the resultant string.
 */
 
+// Long Solution
 function noSpace(x) {
   return x.split(" ").join("");
 }
