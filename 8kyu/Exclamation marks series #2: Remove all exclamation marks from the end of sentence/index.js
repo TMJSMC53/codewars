f28d2def01c25dbf76ad916292d9d8e1264beb35
Triola Jackson <triolajackson@gmail.com>
2022-05-07 16:45:54 -0500

    Create Exclamation marks series #2: Remove all exclamation marks from the end of sentence.js

---
+++ 8kyu/Exclamation marks series #2: Remove all exclamation marks from the end of sentence/index.js
@@ -0,0 +1,17 @@
+/*
+Title: Exclamation marks series #2: Remove all exclamation marks from the end of sentence
+
+Description:
+Remove all exclamation marks from the end of sentence.
+
+Examples:
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+*/
+function remove(string) {
+  return string.replace(/!*$/g, "");
+}
