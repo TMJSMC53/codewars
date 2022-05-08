1174fab7891e1a1ab8bf76ce0b6418831fc53c40
Triola Jackson <triolajackson@gmail.com>
2022-05-08 09:01:03 -0500

    Create Grasshopper - Debug sayHello

---
+++ 8kyu/Grasshopper - Debug sayHello/index.js
@@ -0,0 +1,18 @@
+/*
+Title: Debugging sayHello function
+Description: The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock
+*/
+
+//Long Solution
+// function sayHello(name) {
+//   return "Hello" + ", " + name;
+// }
+
+// Short Solution
+function sayHello(name) {
+  return `Hello, ${name}`;
+}
