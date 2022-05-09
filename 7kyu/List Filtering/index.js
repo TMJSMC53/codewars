3ab991a93c160b0e3de54d8bc781f68ffa40cb3a
Triola Jackson <triolajackson@gmail.com>
2022-05-09 13:12:26 -0500

    Create list filtering.js

---
+++ 7kyu/List Filtering/index.js
@@ -0,0 +1,27 @@
+/*
+Title: List Filtering
+Description: In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+Examples:
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+*/
+
+// Long Solution
+function filter_list(l) {
+  let int = [];
+  for (let i = 0; i < l.length; i++) {
+    if (Number.isInteger(l[i])) {
+      int.push(l[i]);
+    }
+  }
+
+  return int;
+}
+
+// Short Solution
+function filter_list(l) {
+  return l.filter((int) => Number.isInteger(int));
+}
