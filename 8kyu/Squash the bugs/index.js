48fecf9e79aeb220437e17a932fba4f6a99eefa9
Triola Jackson <triolajackson@gmail.com>
2022-05-16 20:57:02 -0500

    Create squash the bugs.js

---
+++ 8kyu/Squash the bugs/index.js
@@ -0,0 +1,11 @@
+/*
+Title: Squash the bugs
+Description: Simple challenge - eliminate all bugs from the supplied code so that the code runs and outputs the expected value. Output should be the length of the longest word, as a number.
+
+There will only be one 'longest' word.
+
+*/
+
+function findLongest(str) {
+  return Math.max(...str.split(" ").map((w) => w.length));
+}
