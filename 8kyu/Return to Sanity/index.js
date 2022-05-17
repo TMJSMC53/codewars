93887b2b6e05126395bf25805f9c1d70103a52ed
Triola Jackson <triolajackson@gmail.com>
2022-05-17 14:50:07 -0500

    Create return to sanity.js

---
+++ 8kyu/Return to Sanity/index.js
@@ -0,0 +1,8 @@
+/*
+Title: Return to Sanity
+Description: This function should return an object, but it's not doing what's intended. What's wrong?
+*/
+function mystery() {
+  let results = { sanity: "Hello" };
+  return results;
+}
