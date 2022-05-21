f2902ec0cfd7ede4503366082b00cbacdede06e3
Triola Jackson <triolajackson@gmail.com>
2022-05-21 14:34:12 -0500

    Create To square(root) or not to square(root).js

---
+++ 8kyu/To square(root) or not to square(root)/index.js
@@ -0,0 +1,20 @@
+/*
+Title: To square(root) or not to square(root)
+Description: Write a method, that will get an integer array as parameter and will process every number from this array.
+
+Return a new array with processing every number of the input-array like this:
+
+If the number has an integer square root, take this, otherwise square the number.
+
+Example
+[4,3,9,7,2,1] -> [2,9,3,49,4,1]
+Notes
+The input array will always contain only positive numbers, and will never be empty or null.
+*/
+function isSquareRootNum(n) {
+  return Number.isInteger(Math.sqrt(n));
+}
+
+function squareOrSquareRoot(array) {
+  return array.map((x) => (isSquareRootNum(x) ? Math.sqrt(x) : x ** 2));
+}
