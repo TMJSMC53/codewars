00053416684d58dfcb813a0eb2d270bc959f1a3f
Triola Jackson <triolajackson@gmail.com>
2022-05-27 14:03:25 -0500

    Create Hex to Decimal.js

---
+++ 8kyu/Hex to Decimal/index.js
@@ -0,0 +1,5 @@
+/*
+Title: Hex to Decimal
+Description: Complete the function which converts hex number (given as a string) to a decimal number.
+*/
+const hexToDec = (hexString) => parseInt(hexString, 16);
