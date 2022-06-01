025da762c61c50201caae8c813013bed22fde886
Triola Jackson <triolajackson@gmail.com>
2022-06-01 11:07:44 -0500

    Create Enumerable Magic #2 - True for Any?.js

---
+++ 8kyu/Enumerable Magic #2 - True for Any?/index.js
@@ -0,0 +1,6 @@
+/*
+Title: Enumerable Magic #2 - True for Any?
+Description: Create an any? (JS: any) function that accepts an array and a block (JS: function), and returns true if the block (/function) returns true for any item in the array. If the array is empty, the function should return false.
+*/
+
+const any = (arr, fun) => arr.some(fun);
