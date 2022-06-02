107ef3d1cdce71ddf35dab3a0a7b7184b5819d9a
Triola Jackson <triolajackson@gmail.com>
2022-06-01 19:08:13 -0500

    Create Training JS #17: Methods of String object--indexOf(), lastIndexOf() and search().js

---
+++ 8kyu/Training JS #17: Methods of String object--indexOf(), lastIndexOf() and search()/index.js
@@ -0,0 +1,35 @@
+/*
+Title: Training JS #17: Methods of String object--indexOf(), lastIndexOf() and search()
+Description: Task
+Coding in function firstToLast, function accept 2 parameters:str and c. str is a string. c is a char.
+
+Please return the gap between the first position of c and the last position of c.
+
+If there are a lot of c in the str, should return a positive integer; If there is only one c in str, should return 0; If there is no c in the str, should return -1. Retrieval should not ignored the case.
+
+for example:
+
+firstToLast("ababc","a") should return 2(2-0)
+firstToLast("ababc","c") should return 0(4-4)
+firstToLast("ababc","d") should return -1
+
+*/
+// const firstToLast = (str, c) => {
+//   let first = str.indexOf(c);
+//   let last = str.lastIndexOf(c);
+//   let gap = last - first;
+
+//   if (str.includes(c)) {
+//     return gap;
+//   } else {
+//     return str.indexOf(c);
+//   }
+// };
+
+const firstToLast = (str, c) => {
+  let first = str.indexOf(c);
+  let last = str.lastIndexOf(c);
+  let gap = last - first;
+
+  return str.includes(c) ? gap : str.indexOf(c);
+};
