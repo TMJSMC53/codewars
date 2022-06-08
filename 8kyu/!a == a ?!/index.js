1c2921a0e2c86ffad4159ec1d96d21de7be878ba
Triola Jackson <triolajackson@gmail.com>
2022-06-07 21:30:29 -0500

    Create !a==a_!.js

--- 8kyu/!a == a ?!/index.js
+++ 8kyu/!a == a ?!/index.js
@@ -1,10 +1,9 @@
 /*
-Title: !a == a ?!
-Description: You task to pass only this tests :
-
+Title: !a==a_!
+Description:
+You task to pass only this tests :
 a == false
 !a == false
 a == !a
-
 */
 const a = "00";
