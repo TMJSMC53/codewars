03c25fa75f135252df4e85284ad04f47ea856841
Triola Jackson <triolajackson@gmail.com>
2022-06-15 18:37:14 -0500

    Create isograms.js

---
+++ 7kyu/Isograms/index.js
@@ -0,0 +1,11 @@
+/*
+Title: Isograms
+Descriptions: An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+Example: (Input --> Output)
+
+"Dermatoglyphics" --> true
+"aba" --> false
+"moOse" --> false (ignore letter case)
+*/
+const isIsogram = (str) => !str.match(/([a-z]).*\1/i);
