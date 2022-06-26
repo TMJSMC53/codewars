682c36aee403551fe47b56c822e9981fe5349c0a
Triola Jackson <triolajackson@gmail.com>
2022-06-26 09:58:46 -0500

    Create Maximum Length Difference.js

---
+++ 7kyu/Maximum Length Difference/index.js
@@ -0,0 +1,28 @@
+/*
+Title: Maximum Length Difference
+Description: You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
+
+Find max(abs(length(x) − length(y)))
+
+If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
+
+Example:
+a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+mxdiflg(a1, a2) --> 13
+Bash note:
+input : 2 strings with substrings separated by ,
+output: number as a string
+
+*/
+function mxdiflg(a1, a2) {
+  let strLen1 = a1.map((strLen) => strLen.length);
+  let strLen2 = a2.map((strLen) => strLen.length);
+
+  if (a1.length === 0 || a2.length === 0) return -1;
+
+  return Math.max(
+    Math.max(...strLen2) - Math.min(...strLen1),
+    Math.max(...strLen1) - Math.min(...strLen2)
+  );
+}
