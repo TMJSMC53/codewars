317c09f2f741100167be7ec4dd8229972462df72
Triola Jackson <triolajackson@gmail.com>
2022-06-30 19:10:51 -0500

    Create max diff - easy.js

---
+++ 7kyu/max diff - easy/index.js
@@ -0,0 +1,32 @@
+/*
+Title: max diff - easy
+Description: You must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter.
+
+lst contains integers, that means it may contain some negative numbers
+if lst is empty or contains a single element, return 0
+lst is not sorted
+[1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
+[1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
+Have fun!
+*/
+
+// Long Solution
+// function maxDiff(list) {
+//   if (!list.length) {return 0}
+//   let min = Math.min(...list)
+
+//   let max = Math.max(...list)
+
+//   let diff = max-min
+//   return diff
+// };
+
+// Short Solution
+function maxDiff(list) {
+  if (!list.length) return 0;
+
+  return Math.max(...list) - Math.min(...list);
+}
+
+console.log(maxDiff([1, 2, 3, 4]));
+console.log(maxDiff([1, 2, 3, -4]));
