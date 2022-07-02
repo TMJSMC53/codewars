1961d9ba1bf08fbf5ff2729ee9e724bf50cb4772
Triola Jackson <triolajackson@gmail.com>
2022-07-01 22:47:02 -0500

    Create esreveR.js

---
+++ 7kyu/esreveR/index.js
@@ -0,0 +1,14 @@
+/*
+Title: esreveR
+Description: Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+
+(the dedicated builtin(s) functionalities are deactivated)
+*/
+
+const reverse = (array) => {
+  let reversed = [];
+
+  for (let i = array.length - 1; i >= 0; i--) reversed.push(array[i]);
+
+  return reversed;
+};
