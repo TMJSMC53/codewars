d0f5de77df0dc0141f7285588fe1f1c58be4b594
Triola Jackson <triolajackson@gmail.com>
2022-07-06 14:31:29 -0500

    Create The Office III - Broken Photocopier.js

---
+++ 7kyu/The Office III - Broken Photocopier/index.js
@@ -0,0 +1,15 @@
+/*
+Title: The Office III - Broken Photocopier
+Description: The bloody photocopier is broken... Just as you were sneaking around the office to print off your favourite binary code!
+
+Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
+
+Given a string of binary, return the version the photocopier gives you as a string.
+
+The Office I - Outed
+The Office II - Boredeom Score
+The Office IV - Find a Meeting Room
+The Office V - Find a Chair
+
+*/
+const broken = (x) => x.replace(/[01]/g, (num) => (num === "1" ? "0" : "1"));
