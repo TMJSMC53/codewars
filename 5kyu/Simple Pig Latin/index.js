3cc046d95c2ebec65913d4748a3a98977b2b68c0
Triola Jackson <triolajackson@gmail.com>
2022-07-14 19:38:01 -0500

    Create Simple Pig Latin.js

---
+++ 5kyu/Simple Pig Latin/index.js
@@ -0,0 +1,16 @@
+/*
+Title: Simple Pig Latin
+Description: Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+Examples
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway !
+*/
+
+const pigIt = (str) => {
+  let marks = ["!", "?", ".", ","];
+  return str
+    .split(" ")
+    .map((word) => word.slice(1) + word[0] + (marks.includes(word) ? "" : "ay"))
+    .join(" ");
+};
